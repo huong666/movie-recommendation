ae39f02da00a7f8c60cb27f80d92fb6d6564bc51
huong666 <55019690+huong666@users.noreply.github.com>
2023-08-04 17:39:16 +0700

    open component home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-import { ComingSoonMovies, TopRatedMovies, MPTVShows } from "@/components/Home";
+import {
+  ComingSoonMovies,
+  TopRatedMovies,
+  MPTVShows,
+  MPMovies,
+} from "@/components/Home";
 
 export default function Home() {
   // // const data = await getData();
@@ -6,15 +11,15 @@ export default function Home() {
   return (
     <main className="min-h-screen py-20 px-20 pt-28">
       <section className="flex flex-col justify-center items-center gap-5">
-        @ts-expect-error Async Server Component
-        {/* <MPMovies /> */}
-        @ts-expect-error Async Server Component
-        {/* <MPTVShows /> */}
+        {/* @ts-expect-error Async Server Component */}
+        <MPMovies />
+        {/* @ts-expect-error Async Server Component */}
+        <MPTVShows />
       </section>
       <section className="grid grid-cols-2 gap-5">
         <div className="col-span-1 ">
-          @ts-expect-error Async Server Component
-          {/* <TopRatedMovies /> */}
+          {/* @ts-expect-error Async Server Component */}
+          <TopRatedMovies />
         </div>
         <div className="col-span-1">
           {/* @ts-expect-error Async Server Component */}
--- src/components/clientThing/clientThing.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useRouter } from "next/router";
-
-export default function Client({ thing }: { thing: string }) {
-  const pathName = useRouter().;
-
-  return <>{thing}</>;
-}
