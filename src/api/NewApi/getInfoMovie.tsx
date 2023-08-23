61b4dca579fe3b995eb96023e553baecb7c8cd2b
nguyễn xuân ân hưởng <h123mn@MacBook-Pro-cua-nguyen.local>
2023-08-23 13:51:02 +0700

    try to add cookies

--- src/api/NewApi/getInfoMovie.tsx
+++ src/api/NewApi/getInfoMovie.tsx
@@ -1,12 +1,19 @@
+import { cookies } from "next/headers";
+
 export async function getInfoMovie(idMovie: string) {
   // const url = "http://127.0.0.1:3001/title/" + idMovie;
   const url =
     "https://imdb-api.nguyenxuananhuong541.workers.dev/title/" + idMovie;
 
   try {
-    const result = await fetch(url).then((res) => {
-      return res.json();
-    });
+    const result = await fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        return data;
+      });
+    // console.log("data from cookies", cookies().get("data"));
     return result;
   } catch (error) {
     console.log(error);
