7c7ef73794adb52701a5a524bcd02132d037f593
nguyễn xuân ân hưởng <h123mn@MacBook-Pro-cua-nguyen.local>
2023-08-24 18:14:36 +0700

    remove recommed

---
+++ src/app/genre/page.tsx
@@ -0,0 +1,9 @@
+import GenreMovie from "@/components/GenreMovie";
+
+export default function GenrePage() {
+  return (
+    <main className="py-32">
+      <GenreMovie />
+    </main>
+  );
+}
---
+++ src/components/GenreMovie/GenreMovie.tsx
@@ -0,0 +1,150 @@
+"use client";
+
+import { handleGenreMoviesList, handleRecommendMovies } from "@/lib/serverFun";
+import { useEffect, useState } from "react";
+import { MovieGrid } from "../MoviesRender";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { AiOutlineCaretDown } from "react-icons/ai";
+
+type TypeGenreList =
+  | "action"
+  | "horror"
+  | "popular"
+  | "adventure"
+  | "comedy"
+  | "animation"
+  | "biography"
+  | "crime"
+  | "romance";
+
+export default function GenreMovie() {
+  const [infoMovies, setInfoMovies] = useState(undefined);
+  const [storeLocal, setStoreLocal] = useState<boolean>(false);
+  const [typeList, setTypeList] = useState<TypeGenreList>("action");
+
+  async function handleGetRecommendMovies(idMovie: string) {
+    setInfoMovies(undefined);
+    const data = await handleRecommendMovies(idMovie);
+    setInfoMovies(data);
+  }
+
+  async function handleGetGenreMoviesData(typeList: TypeGenreList) {
+    setInfoMovies(undefined);
+    const data = await handleGenreMoviesList(typeList);
+    setInfoMovies(data);
+  }
+
+  useEffect(() => {
+    handleGetGenreMoviesData(typeList);
+  }, [typeList]);
+
+  return (
+    <section className="py-5 2xl:px-56 xl:px-36 lg:px-12 px-4">
+      <div className="flex flex-col gap-10">
+        <div className="flex items-center justify-between">
+          <h1 className="md:text-xl text-base font-medium">
+            RECOMMEND MOVIES THAT YOU MIND BE LIKED
+          </h1>
+          <Popover>
+            <PopoverTrigger className="border border-black dark:border-white p-2 rounded-lg flex gap-3 items-center w-60 justify-between">
+              {typeList == "action"
+                ? "Action"
+                : typeList == "horror"
+                ? "Horror"
+                : typeList == "popular"
+                ? "Popular"
+                : typeList == "adventure"
+                ? "Adventure"
+                : typeList == "comedy"
+                ? "Comedy"
+                : typeList == "animation"
+                ? "Animation"
+                : typeList == "biography"
+                ? "Biography"
+                : typeList == "crime"
+                ? "Crime"
+                : "Romance"}
+              <AiOutlineCaretDown />
+            </PopoverTrigger>
+            <PopoverContent className="w-full mt-1">
+              <ul className="flex flex-col">
+                <li
+                  className="hover:bg-slate-200 dark:hover:bg-slate-800 py-2 px-3 cursor-pointer"
+                  onClick={() => setTypeList("action")}
+                >
+                  Action
+                </li>
+                <li
+                  className="hover:bg-slate-200 dark:hover:bg-slate-800 py-2 px-3 cursor-pointer"
+                  onClick={() => setTypeList("horror")}
+                >
+                  Horror
+                </li>
+                <li
+                  className="hover:bg-slate-200 dark:hover:bg-slate-800 py-2 px-3 cursor-pointer"
+                  onClick={() => setTypeList("popular")}
+                >
+                  Popular
+                </li>
+                <li
+                  className="hover:bg-slate-200 dark:hover:bg-slate-800 py-2 px-3 cursor-pointer"
+                  onClick={() => setTypeList("adventure")}
+                >
+                  Adventure
+                </li>
+                <li
+                  className="hover:bg-slate-200 dark:hover:bg-slate-800 py-2 px-3 cursor-pointer"
+                  onClick={() => setTypeList("comedy")}
+                >
+                  Comedy
+                </li>
+                <li
+                  className="hover:bg-slate-200 dark:hover:bg-slate-800 py-2 px-3 cursor-pointer"
+                  onClick={() => setTypeList("animation")}
+                >
+                  Animation
+                </li>
+                <li
+                  className="hover:bg-slate-200 dark:hover:bg-slate-800 py-2 px-3 cursor-pointer"
+                  onClick={() => setTypeList("biography")}
+                >
+                  Biography
+                </li>
+                <li
+                  className="hover:bg-slate-200 dark:hover:bg-slate-800 py-2 px-3 cursor-pointer"
+                  onClick={() => setTypeList("crime")}
+                >
+                  Crime
+                </li>
+                <li
+                  className="hover:bg-slate-200 dark:hover:bg-slate-800 py-2 px-3 cursor-pointer"
+                  onClick={() => setTypeList("romance")}
+                >
+                  Romance
+                </li>
+              </ul>
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div className="min-h-screen">
+          {/* {storeLocal == true ? (
+          "We dont have enough data to recommend"
+        ) : infoMovies == undefined ? (
+          "Loading"
+        ) : (
+          <MovieGrid infoMovies={infoMovies} />
+        )} */}
+          {infoMovies == undefined ? (
+            "Loading"
+          ) : (
+            <MovieGrid infoMovies={infoMovies} />
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
